ccb4bde5c8487ce623ae97ea6a31cf29b368e0ba
Ana Cristina <72415738@continental.edu.pe>
2024-09-09 23:22:05 -0500

    Se añadieron las pruebas unitarias

--- Geometria.py
+++ Geometria.py
@@ -20,3 +20,39 @@ class Geometria:
 
     def perimetro_rectangulo(self, ancho, alto):
         return self.perimetro(ancho, alto)
+
+
+#Realizando pruebas
+geo = Geometria()
+
+def probar_cuadrado():
+    print("---- PRUEBAS PARA CUADRADO ----")
+    lado = 4
+    print(f"Área del cuadrado (lado={lado}): {geo.area_cuadrado(lado)}")
+    lado = 5
+    print(f"Área del cuadrado (lado={lado}): {geo.area_cuadrado(lado)}")
+
+    lado = 4
+    print(f"Perímetro del cuadrado (lado={lado}): {geo.perimetro_cuadrado(lado)}")
+    lado = 5
+    print(f"Perímetro del cuadrado (lado={lado}): {geo.perimetro_cuadrado(lado)}")
+
+
+def probar_rectangulo():
+    print("---- PRUEBAS PARA RECTÁNGULO ----")
+    ancho, alto = 4, 6
+    print(f"Área del rectángulo (ancho={ancho}, alto={alto}): {geo.area_rectangulo(ancho, alto)}")
+    ancho, alto = 3, 7
+    print(f"Área del rectángulo (ancho={ancho}, alto={alto}): {geo.area_rectangulo(ancho, alto)}")
+
+    ancho, alto = 4, 6
+    print(
+        f"Perímetro del rectángulo (ancho={ancho}, alto={alto}): {geo.perimetro_rectangulo(ancho, alto)}")
+    ancho, alto = 3, 7
+    print(
+        f"Perímetro del rectángulo (ancho={ancho}, alto={alto}): {geo.perimetro_rectangulo(ancho, alto)}")
+
+
+probar_cuadrado()
+print()
+probar_rectangulo()
