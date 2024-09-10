83ee23746ace88d9f204cb5d0e86c5f6196a0e52
Ana Cristina <72415738@continental.edu.pe>
2024-09-09 23:03:19 -0500

    Refactorización del código

--- Geometria.py
+++ Geometria.py
@@ -1,12 +1,22 @@
 class Geometria:
-    def area_cuadrado(self, lado):
-        return lado * lado
-
-    def area_rectangulo(self, ancho, alto):
+    def area(self, ancho, alto=None):
+        if alto is None:
+            alto = ancho
         return ancho * alto
 
+    def perimetro(self, ancho, alto=None):
+        if alto is None:
+            alto = ancho
+        return 2 * (ancho + alto)
+
+    def area_cuadrado(self, lado):
+        return self.area(lado)
+
+    def area_rectangulo(self, ancho, alto):
+        return self.area(ancho, alto)
+
     def perimetro_cuadrado(self, lado):
-        return 4 * lado
+        return self.perimetro(lado)
 
     def perimetro_rectangulo(self, ancho, alto):
-        return 2 * (ancho + alto)
+        return self.perimetro(ancho, alto)
